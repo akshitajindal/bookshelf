dac803e2dd7a5a25e10c12a632fba29035677540
Akshita Jindal <akshita.jindal@sprinklr.com>
2023-03-08 00:09:21 +0530

    exercise

--- src/setupTests.extra-2.js
+++ src/setupTests.extra-2.js
@@ -1,5 +1,7 @@
 import {server} from 'test/server'
 
+// this file is automatically used by jest, hence running these commands here makes sure that all our test will be configured this way.
+
 // enable API mocking in test runs using the same request handlers
 // as for the client-side mocking.
 beforeAll(() => server.listen())
